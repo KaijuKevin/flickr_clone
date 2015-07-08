c797c738895176262b258a1fbe6e4d1542870ec9
Leon Zagorouiko & Kevin Chen <student@epicodus.com>
2015-07-08 16:37:00 -0700

    Add mailer and styles

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,9 @@
 class UserMailer < ApplicationMailer
+
+  def signup_confirmation(user)
+    @user = user
+    @greeting = "Hi"
+
+    mail to: @user.email, subject: "Signup confirmation"
+  end
 end
