dc3d15af3f147e2dec99ef02f8a110cc223c68a2
Kevin Chen & Leon Zagorouiko <student@epicodus.com>
2015-07-09 16:18:51 -0700

    Fix for heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>"}, :default_url => "missing.png"
   validates_attachment_content_type :avatar, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
-  after_create :send_signup_confirmation
+  # after_create :send_signup_confirmation
 
   def send_signup_confirmation
     UserMailer.signup_confirmation(self).deliver_now
